6c755311c155bd49d25fe068058f378ab2f6fe30
vladi <vzaimov@yahoo.com>
2020-01-13 23:44:21 +0200

    test some dynamic routes

--- urunner_app.py
+++ urunner_app.py
@@ -6,11 +6,14 @@ app = Flask(__name__)
 # use the decorator pattern to
 # link the view function to a url
 
+app.config["DEBUG"] = True
+
+
 
 @app.route("/")
 # define the view using a function, which returns a string
 def hello_uruner():
-    return "Hello, Urunner!"
+    return "Hello, Urunner!!!"
 
 
 @app.route("/hello")
@@ -22,6 +25,25 @@ def hello_world():
 @app.route("/test/<search_query>")
 def search(search_query):
     return search_query
+
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1)
+    return "correct"
+
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return "correct"
+
+
+# dynamic route that accepts slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+    print(value)
+    return "correct"
 
 
 # start the development server using the run() method
